afdf8ce345ef1d01bb1fad6d9ec6ed3cdaa42d73
Nikhil Singh <nikhil.singh@abans.co.in>
2025-09-03 22:27:30 +0530

    today's commit

--- SideCode.cpp
+++ SideCode.cpp
@@ -143,6 +143,37 @@ public:
     }
 };
 
+class Solution {
+private:
+	bool cmp(vector<int> &a, vector<int> &b) {
+		if(a[0] == b[0]) return a[1] > b[1];
+		else return a[0] < b[0];
+	}
+
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), cmp);
+
+		int ans = 0;
+		for(int i=0; i<points.size(); i++) {
+			for(int j=i+1; j<points.size(); j++) {
+				if(points[j][1] <= points[i][1]) {
+					bool isValidPair = true;
+					for(int k=i+1; k<j; k++) {
+						if(points[k][1] <= points[i][1] && points[k][1] >= points[j][1]) {
+							isValidPair = false;
+							break;
+						}
+					}
+					if(isValidPair) ans++;
+				}
+			}
+		}
+
+		return ans;
+    }
+};
+
 int main() {
     Solution1 s;
 	vector<vector<int>> v = {{0,1,1,1},{1,1,1,1},{0,1,1,1}};
